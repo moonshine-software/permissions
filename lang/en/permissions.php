860cc6e352291bef02e4bf7be7f6de6da4170796
ForwardDev <167892602+TheForwardDev@users.noreply.github.com>
2025-02-04 17:52:03 +0200

    add: translate ability
    Added translations of abilities

--- lang/en/permissions.php
+++ lang/en/permissions.php
@@ -5,4 +5,12 @@ return [
     "all" => "All",
     "save" => "Save",
     "toggle_off_on_all" => "Toggle off/on all",
+    "viewAny" => "View index page",
+    "view" => "View detail page",
+    "create" => "Create item",
+    "update" => "Edit item",
+    "delete" => "Delete item",
+    "massDelete" => "Mass delete items",
+    "restore" => "Restore item",
+    "forceDelete" => "Permanent delete items",
 ];
